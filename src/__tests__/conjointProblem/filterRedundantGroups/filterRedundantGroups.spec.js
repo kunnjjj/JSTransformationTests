3243770754705e781427fcd3cd0c9354fab72fc2
kunj.arora@sprinklr.com <kunj.arora@sprinklr.com>
2024-08-28 08:54:34 +0530

    filter redundant groups

---
+++ src/__tests__/conjointProblem/filterRedundantGroups/filterRedundantGroups.spec.js
@@ -0,0 +1,31 @@
+const filterRedundantExclusionGroups = require("./implementation");
+const {
+  MOCK_EXCLUSION_GROUPS_0,
+  MOCK_EXCLUSION_GROUPS_1,
+  MOCK_EXCLUSION_GROUPS_2,
+  MOCK_EXCLUSION_GROUPS_2_RESULT,
+} = require("./__mocks__");
+
+describe("filterRedundantExclusionGroups", () => {
+  test("should return same groups if no overlap", () => {
+    const result = filterRedundantExclusionGroups({
+      exclusionGroups: MOCK_EXCLUSION_GROUPS_0,
+    });
+
+    expect(result).toEqual(MOCK_EXCLUSION_GROUPS_0);
+  });
+
+  test("should return same groups if there is partial overlap", () => {
+    const result = filterRedundantExclusionGroups({
+      exclusionGroups: MOCK_EXCLUSION_GROUPS_1,
+    });
+    expect(result).toEqual(MOCK_EXCLUSION_GROUPS_1);
+  });
+
+  test("should filter redundant groups when overlapping groups present", () => {
+    const result = filterRedundantExclusionGroups({
+      exclusionGroups: MOCK_EXCLUSION_GROUPS_2,
+    });
+    expect(result).toEqual(MOCK_EXCLUSION_GROUPS_2_RESULT);
+  });
+});

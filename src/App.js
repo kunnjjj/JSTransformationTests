042b0e44cc97f7f47e9238a736c90104547522ff
kunj.arora@sprinklr.com <kunj.arora@sprinklr.com>
2024-09-02 18:33:53 +0530

    initializes react proj

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Please attempt "src/candidateSolutions/filterRedundantRules" (problem
+        description in README.md)
+      </header>
+    </div>
+  );
+}
+
+export default App;
